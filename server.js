d3a559613a3e1b56112433e48171aa2656c21652
Maribel <minicatsCB@users.noreply.github.com>
2019-06-23 01:16:01 +0200

    Implement server

--- server.js
+++ server.js
@@ -8,14 +8,42 @@ app.use(express.urlencoded({
     extended: false
 }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+    let movies = controller.getAllMovies().then(movies => {
+        res.send(movies);
+    });
+});
 
-app.get("/movie/:id", (req, res) => {});
+app.get("/movie/:id", (req, res) => {
+    controller.getMovieById(req.params.id).then(movie => {
+        res.send(movie);
+    });
+});
 
-app.post("/movies", (req, res) => {});
+app.post("/movies", (req, res) => {
+    controller.saveMovie(req.body.title).then(movie => {
+        if (movie) {
+            res.status(200).send(movie);
+        } else {
+            res.status(510).send({
+                error: "Cannot add the movie. The movie already exists in the database."
+            });
+        }
+    });
+});
 
-app.delete("/movies", (req, res) => {});
+app.delete("/movies", (req, res) => {
+    controller.deleteMovie(req.body.id).then(() => {
+        res.status(200).send({
+            message: "Movie deleted succesfully"
+        });
+    });
+});
 
-app.put("/movies", (req, res) => {});
+app.put("/movies", (req, res) => {
+    controller.updateMovie(req.body.id, req.body.title).then(updateMovie => {
+        res.status(200).send(updateMovie);
+    });
+});
 
 app.listen(port, () => console.log("Listening on port " + port));
